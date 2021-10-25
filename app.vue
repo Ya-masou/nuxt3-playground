7a1afbe4c338b41536ab77db1f7ce5fe996ab213
Yama-Souichirou <souichirou19960210@gmail.com>
2021-10-25 10:09:08 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="border-b-2">
+    <div class="p-4 container mx-auto">
+      <div class="flex justify-between">
+        <div>
+          Nuxt3 すぶり
+        </div>
+        <div>
+          <ul class="flex gap-x-8">
+            <li>いぬ</li>
+            <li>ねこ</li>
+            <li>とり</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- Remove this component to get started! -->
+  <main class="p-4 container mx-auto">
+    <NuxtPage />
+  </main>
+</template>
+
